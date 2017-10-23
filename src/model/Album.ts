80026447e796b6a343371140e7b0d782a83bd1a7
Énekes Ábel <enekes.abel@codecluster.io>
2017-10-23 17:43:24 +0200

    album constructor fix

--- src/model/Album.ts
+++ src/model/Album.ts
@@ -2,6 +2,7 @@ import {Document} from './Document';
 import {AlbumOptions} from './AlbumOptions';
 import {DocumentType} from './DocumentType';
 import {DocumentFactory} from './DocumentFactory';
+import {DocumentOptions} from './DocumentOptions';
 
 export class Album extends Document implements AlbumOptions {
   private _type: DocumentType = DocumentType.ALBUM;
@@ -11,14 +12,17 @@ export class Album extends Document implements AlbumOptions {
   constructor(options: AlbumOptions) {
     super(options);
     options.children.forEach(document => {
-      this._children.push(this.factory.getDocument(document));
+      const documentOption:DocumentOptions = document;
+      documentOption.parent = this;
+      document.parent = this;
+      this._children.push(this.factory.getDocument(documentOption));
     });
   }
 
   get type(): DocumentType {
     return this._type;
   }
-í
+
   get children(): Document[] {
     return this._children;
   }
